acdf44404a772e1c41096abb7b379a4c6605c71d
vitoralveschaves <vitorchavesalv@gmail.com>
2025-01-21 22:57:11 -0300

    feat: add ServiceItem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,15 +1,25 @@
+"use client"
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarberShopItemProps {
   barbershop: Barbershop;
 }
 
 const BarberShopItem = ({ barbershop }: BarberShopItemProps) => {
+
+  const route = useRouter();
+
+  const handleBookingClick = () => {
+    route.push(`/barbershops/${barbershop.id}`);
+  }
+
   return (
     <Card className="min-w-[165px] max-w-[165px] rounded-2xl">
       <CardContent className="p-0">
@@ -25,7 +35,7 @@ const BarberShopItem = ({ barbershop }: BarberShopItemProps) => {
         <div className="px-3 pb-3">
           <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
-          <Button className="w-full mt-3" variant={'secondary'}>Reservar</Button>
+          <Button className="w-full mt-3" variant={'secondary'} onClick={handleBookingClick}>Reservar</Button>
         </div>
       </CardContent>
     </Card>
---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,43 @@
+import { db } from "@/app/_lib/prisma";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+
+interface BarbershopDetailsProps {
+  params: {
+    id?: string
+  }
+}
+
+const BarbershopDetails = async ({ params }: BarbershopDetailsProps) => {
+
+  if (!params.id) {
+    return null
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id
+    },
+    include: {
+      services: true
+    }
+  })
+
+  if (!barbershop) {
+    return null;
+  }
+
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+
+      <div className="px-5 py-6 flex flex-col gap-4">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopDetails;

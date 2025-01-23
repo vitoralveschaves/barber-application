d8fe7798a764b2329b22adea852b2996650f1803
vitoralveschaves <vitorchavesalv@gmail.com>
2025-01-23 19:57:04 -0300

    feat: add initial service booking

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -36,7 +36,7 @@ const BarbershopDetails = async ({ params }: BarbershopDetailsProps) => {
 
       <div className="px-5 py-6 flex flex-col gap-4">
         {barbershop.services.map((service) => (
-          <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
+          <ServiceItem key={service.id} service={service} barbershop={barbershop} isAuthenticated={!!session?.user} />
         ))}
       </div>
     </div>
